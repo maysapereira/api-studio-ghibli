0777157aa06f204ae33349c23ce912294a36fc80
Maysa Pereira <maysalvespereira@gmail.com>
2022-04-01 03:03:27 -0300

    :card_file_box: (build): adiciona banco de dados

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { openDB } from "./configDB.js";
 
 const app = express()
 const port = 3000
@@ -6,3 +7,5 @@ const port = 3000
 app.listen(port, () => {
     console.log(`API rodando na porta ${port}`)
 })
+
+openDB()
